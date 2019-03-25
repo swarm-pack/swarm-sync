38a7ddafcb06e801e2cc83bb9fc9be0fbb004ea7
Kevin B <kevbuk@gmail.com>
2019-03-25 19:34:15 +0800

    chmod id_rsa 600 not 700

--- start.sh
+++ start.sh
@@ -6,9 +6,13 @@ source ./env_secrets_expand.sh
 if [ "$SSH_PRIVATE_KEY" != "false" ]
 then
 echo "CREATE ID_RSA FROM SSH_PRIVATE_KEY"
-echo -e "${SSH_PRIVATE_KEY}" > /root/.ssh/id_rsa
-chmod 700 /root/.ssh/id_rsa
+echo -e "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
 fi
+
+echo "STARTING SSH AGENT & LOADING KEYS"
+eval "$(ssh-agent -s)"
+ssh-add -l -E md5
 
 echo "START THE APP..."
 node src/index.js "$@"
