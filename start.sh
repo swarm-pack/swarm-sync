c27ee7d14115f90bfe41b525e2ef2a30d425726c
Kevin B <kevbuk@gmail.com>
2019-03-25 17:39:13 +0800

    echo newlines in SSH_PRIVATE_KEY to id_rsa

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ source ./env_secrets_expand.sh
 if [ "$SSH_PRIVATE_KEY" != "false" ]
 then
 echo "CREATE ID_RSA FROM SSH_PRIVATE_KEY"
-echo "${SSH_PRIVATE_KEY}" > /root/.ssh/id_rsa
+echo -e "${SSH_PRIVATE_KEY}" > /root/.ssh/id_rsa
 chmod 700 /root/.ssh/id_rsa
 fi
 
