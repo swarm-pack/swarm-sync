c97a24304f00b19d4b9db57596fff1137449faab
Kevin B <kevbuk@gmail.com>
2019-05-05 20:33:28 +0800

    refactor SIGTERM exit code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 #! /usr/bin/env node
-
-const nodeCleanup = require('node-cleanup');
 const log = require('./utils/logger');
 // Init logging
 log.setLevel(process.env.SWARM_SYNC_LOGLEVEL || 2);
@@ -12,47 +10,44 @@ const { checkAndUpdateImages } = require('./sync/serviceImages');
 let exit = false; // exit flag for graceful exit
 let active = true; // Set to false when 'waiting' between updates, to indicate we can safely exit
 
-nodeCleanup((exitCode, signal) => {
+process.on('SIGTERM', () => {
   // Received signal to terminate, likely Docker updating the service
-  log.warn(`Received signal ${signal} (exitCode ${exitCode})`);
-  if (signal === 'SIGTERM' && active) {
-    log.warn('Waiting for operations to complete before exit...');
+  log.warn(`Received SIGTERM signal`);
+  if (active) {
     exit = true;
-    return false;
+    log.warn('Waiting for operations to complete before exit...');
+  } else {
+    process.exit(0);
   }
-  // Somethimes process.exit elsewhere gets stuck here and never ends
-  // For now, adding this kill to be sure
-  process.kill(process.pid, 'SIGKILL');
-  return true;
 });
 
 async function startUpdates() {
   try {
     active = true;
-    await checkAndDeployRepo();
-
-    // OK to exit here if needed as images can be updated in next process
-    if (!exit) {
-      await checkAndUpdateImages();
-    }
+    if (!exit) await checkAndDeployRepo();
+    if (!exit) await checkAndUpdateImages();
     active = false;
-    if (exit) {
-      log.warn('Operations complete, exiting');
-      process.exit(0);
-    }
-    if (!config.bootstrap) {
-      log.info(
-        `\n -- Waiting ${config.updateInterval / 1000} seconds for next scan. -- \n`
-      );
-      setTimeout(startUpdates, config.updateInterval);
-    } else {
-      log.info('Bootstrap complete');
-      process.exit(0);
-    }
   } catch (error) {
     log.error('Fatal unhandled exception');
     log.error(error);
     process.exit(1);
+  }
+
+  // Exit if we got a SIGTERM earlier
+  if (exit) {
+    log.warn('Operations complete, exiting');
+    process.exit(0);
+  }
+
+  // Either loop again or exit if bootstrap mode
+  if (!config.bootstrap) {
+    log.info(
+      `\n -- Waiting ${config.updateInterval / 1000} seconds for next scan. -- \n`
+    );
+    setTimeout(startUpdates, config.updateInterval);
+  } else {
+    log.info('Bootstrap complete');
+    process.exit(0);
   }
 }
 
